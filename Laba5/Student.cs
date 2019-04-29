f9242b030928ea057e16f68a40064502c2ff8a72
Shablevskiy Nikita <chitbomb@gmail.com>
2019-04-29 21:39:53 +0300

    Добавление  оставшихся файлов

---
+++ Laba5/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            // TODO   you SHOULD IMPLEMENT constructor with exactly this signature
+            // lineWithAllData is string contating all data about one student, as described in statement
+        }
+    }
+}
